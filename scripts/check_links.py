b73833d16ac1c1cb6ff017282fd6848ea57a07f2
ImanM02 <imanm1381@gmail.com>
2026-07-21 16:35:43 -0700

    Check all site repo links

--- scripts/check_links.py
+++ scripts/check_links.py
@@ -9,8 +9,13 @@ required = [
     "https://t.me/AbzumsAI",
     "https://github.com/AbzumsAI/Assignments",
     "https://github.com/AbzumsAI/Notebooks",
+    "https://github.com/AbzumsAI/Slides",
     "https://github.com/AbzumsAI/Problems",
+    "https://github.com/AbzumsAI/PreTest",
+    "https://github.com/AbzumsAI/Latex",
     "https://github.com/AbzumsAI/QA-Bot",
+    "https://github.com/AbzumsAI/.github",
+    "https://github.com/AbzumsAI/abzumsai.github.io",
 ]
 
 missing = [link for link in required if link not in links]
